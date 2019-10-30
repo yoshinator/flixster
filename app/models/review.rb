3c76a1030cf5c69261ead40b5adfb9f8aa90f6d9
yoshinator <yoanante@gmail.com>
2019-10-29 21:11:01 -0400

    has many side of relationship

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  STARS = [0,1,2,3,4,5,6,7,8,9,10]
+  validates :name, presence: true 
+  validates :comment, length: { minimum: 4 }
+  validates :stars, numericality: {greater_than_or_equal_to:0, less_than_or_equal_to: 10}
+  validates :stars, inclusion: {in: STARS, message: "Must be between 0 and 10"}
 end

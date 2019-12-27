727958a1f455b62f607140319ee501d85b804cb3
yoshinator <yoanante@gmail.com>
2019-12-27 12:11:17 -0500

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: :true 
+  validates :email, format: { with: /\S+@\S+\.\S+/ },
+            uniqueness: {case_sensitive: false}
+  validates :password, length: { minimum: 9, allow_blank: true }
 end

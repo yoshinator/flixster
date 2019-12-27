ef2844e2a1efcb526d3e5842d71ba02f7bf39163
yoshinator <yoanante@gmail.com>
2019-12-27 13:41:49 -0500

    added user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   root "movies#index"
   resources :movies do
     resources :reviews
   end
+  
+  resources :users
+  get "signup", to: "users#new"
 end
